2753d001c14ae77a1156f85fda1ef53be54eb456
James Dunkerley <jdunkerley@users.noreply.github.com>
2023-08-17 12:14:31 +0100

    Push updated grouping and list

--- src/data/Grouping.ts
+++ src/data/Grouping.ts
@@ -42,30 +42,34 @@ export const DEFAULT = new Grouping('Default', '#A997AD', '')
 // Ordering is approximately big to small.
 // Affect most data at top down to affect least data at bottom.
 
+// Table operations act on one or more table producing a new table - Join, Union, etc.
+// Restructuring - remove columns, rename columns, filter rows, select columns, take and drop, etc., tokenise
+// Value Operations - replace, parse, cast, etc. (one (or more) value within a row (look ahead/behind) maps to one value)
+// Positioning of Input/Output...
+
 var i: number = 0
 var math_i: number = 0
+var table_i: number = 0
 const GROUPINGS: Grouping[] = [
-    new Grouping('Input / Output', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
+    new Grouping('Input', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
     new Grouping('Constants', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
     new Grouping('Metadata', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Table Operations', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Statistics', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Find / Lookup', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Column Operations', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Row Operations', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Conversion / Parsing', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
+    new Grouping('Table Calculations', GROUPING_COLORS[table_i = i++ % GROUPING_COLORS.length]),
+    new Grouping('Statistics', GROUPING_COLORS[table_i]),
+    new Grouping('Selections', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
+    new Grouping('Conversions', GROUPING_COLORS[i % GROUPING_COLORS.length]),    
+    new Grouping('Calculations', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
     new Grouping('Math', GROUPING_COLORS[math_i = (i++ % GROUPING_COLORS.length)]),
-    new Grouping('Math : Rounding', GROUPING_COLORS[math_i]),
-    new Grouping('Math : Trigonmetry', GROUPING_COLORS[math_i]),
-    new Grouping('Math : Bitwise', GROUPING_COLORS[math_i]),
+    new Grouping('Rounding', GROUPING_COLORS[math_i]),
+    new Grouping('Trigonmetry', GROUPING_COLORS[math_i]),
+    new Grouping('Random', GROUPING_COLORS[math_i]),
+    new Grouping('Bitwise', GROUPING_COLORS[math_i]),
     new Grouping('Text', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Date / Time', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
+    new Grouping('DateTime', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
     new Grouping('Logical', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Process / File', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
     new Grouping('Operators', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Cloud / Web', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Data Generation', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
-    new Grouping('Error Handling', GROUPING_COLORS[i++ % GROUPING_COLORS.length])
+    new Grouping('Output', GROUPING_COLORS[i++ % GROUPING_COLORS.length]),
+    new Grouping('Errors', GROUPING_COLORS[i++ % GROUPING_COLORS.length])
 ]
 
 export function getGrouping(name: string): Grouping {
